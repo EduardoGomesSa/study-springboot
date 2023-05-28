f89ef5b2651082f8374af049c2d22b2334e51e89
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-28 00:11:16 -0300

    configuração do docker no projeto e mudança na hierarquia das pastas

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,54 +1,26 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
-import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
-import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 
 @RestController
-@RequestMapping("animes")
-@RequiredArgsConstructor
+@RequestMapping("anime")
 public class AnimeController {
-    private final DateUtil dateUtil;
-    private final AnimeService animeService;
-    private static final Logger logger = LogManager.getLogger(AnimeController.class);
-
-    @GetMapping
-    public ResponseEntity<List<Anime>> list(){
-        logger.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        //System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-
-        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+    private DateUtil dateUtil;
+    public AnimeController(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
     }
-
-    @GetMapping(path = "/{id}")
-    public ResponseEntity<Anime> findById(@PathVariable long id){
-        return ResponseEntity.ok(animeService.findById(id));
-    }
-
-    @PostMapping
-    public ResponseEntity<Anime> Save(@RequestBody Anime anime){
-        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
-    }
-
-    @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable long id){
-        animeService.delete(id);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
-
-    @PutMapping
-    public ResponseEntity<Anime> replace(@RequestBody Anime anime){
-        animeService.replace(anime);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @GetMapping(path = "/list")
+    public List<Anime> list(){
+        //log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("One Piece"), new Anime("Naruto"), new Anime("CDZ"));
     }
 }
--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++
@@ -1,9 +0,0 @@
-package academy.devdojo.springboot2.repository;
-
-import academy.devdojo.springboot2.domain.Anime;
-
-import java.util.List;
-
-public interface AnimeRepository {
-    List<Anime> listAll();
-}
