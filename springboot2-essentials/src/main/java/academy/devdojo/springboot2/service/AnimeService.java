432e5ae84e5f84c670157b116a12935eb6305719
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-31 16:11:01 -0300

    criação de uma classe para tratamento de exceções

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.exception.BadRequestException;
 import academy.devdojo.springboot2.mapper.AnimeMapper;
 import academy.devdojo.springboot2.repository.AnimeRepository;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
@@ -19,7 +20,7 @@ public class AnimeService {
 
     public Anime findByIdOrThrownBadRequestException(Long id){
         return animeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+                .orElseThrow(() -> new BadRequestException("Anime not found"));
     }
 
     public List<Anime> list(){
