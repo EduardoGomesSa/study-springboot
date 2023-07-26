e6744811ef7e226a9f2308f36dccf91bb6c56c01
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-07-25 23:41:10 -0300

    configuração de segurança do sistema

---
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package academy.devdojo.springboot2.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("Eduardo")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("devdojo")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER");
+    }
+}
