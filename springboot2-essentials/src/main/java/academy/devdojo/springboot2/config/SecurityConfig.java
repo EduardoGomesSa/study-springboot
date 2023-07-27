27ddd580c2841c59d469f3c0124b49f3011733c0
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-07-26 23:09:00 -0300

    resolução da exception 403 em post

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -6,12 +6,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
