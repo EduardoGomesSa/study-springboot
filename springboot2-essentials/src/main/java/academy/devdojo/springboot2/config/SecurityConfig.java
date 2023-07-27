0c977c6263ccdb5f74aa6b100daefa7ba5d8dd28
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-07-26 23:25:19 -0300

    controle de usuario para acessar métodos especificos

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
